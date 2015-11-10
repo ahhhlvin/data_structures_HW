45367f18566b8013fe34c98e1fb2d17ffdd4a28a
ahhhlvin <alvkuang@gmail.com>
2015-11-09 21:58:21 -0500

    added network homework

---
+++ src/SpiderTest.java
@@ -0,0 +1,19 @@
+/**
+ * Created by alvin2 on 11/9/15.
+ * Alvin Kuang
+ * C4Q Access Code 2.1
+ */
+public class SpiderTest
+{
+    /**
+     * This is our test. It creates a spider (which creates spider legs) and crawls the web.
+     *
+     * @param args
+     *            - not used
+     */
+    public static void main(String[] args)
+    {
+        Spider spider = new Spider();
+        spider.search("http://jsoup.org/download", "jar");
+    }
+}
